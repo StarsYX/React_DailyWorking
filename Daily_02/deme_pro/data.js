ba5739e1d36ceaef9e697888d45770ef7e3c32ee
赵恒星 <zhaohx10@asiainfo.com>
2023-05-07 22:35:40 +0800

    添加table测试

---
+++ Daily_02/deme_pro/data.js
@@ -0,0 +1,31 @@
+const booksList = [
+    {
+        id: 1,
+        name:'《鸣人物语》',
+        date: '2002-4',
+        price: 188.89,
+        count: 999
+    },
+    {
+        id: 2,
+        name:'《卡卡西传》',
+        date: '1999-9',
+        price: 66.00,
+        count: 33
+    },
+    {
+        id: 3,
+        name:'《自来也传》',
+        date: '2022-6',
+        price: 89.21,
+        count: 5
+    },
+    {
+        id: 4,
+        name:'《sasuke列传》',
+        date: '2018-7',
+        price: 87.34,
+        count: 65
+    }
+
+]
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # React_DailyWorking
 React学习
-## 1.react中条件渲染
+## 1.react中条件渲染（03_condition.html）
 * if条件判断
 * 三元运算符
 * &&与运算符
